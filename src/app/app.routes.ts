87ff4e567cc6732be116d0296e8400c5351ff93c
Sviatoslav Kunynets <kuninsoft@gmail.com>
2025-11-23 19:35:41 +0100

    Added 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HabitCounterPageComponent } from './pages/habit-counter-page/habit-counter-page.component';
+import { StatisticsPageComponent } from './pages/statistics-page/statistics-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HabitCounterPageComponent
+    },
+    {
+        path: 'statistics',
+        component: StatisticsPageComponent
+    },
+    {
+        path: '**',
+        component: NotFoundPageComponent
     }
 ];
